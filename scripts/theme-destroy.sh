96628e78bcb5490c8d0f001fdbe3e181724cbd05
pjliddy <pjliddy@gmail.com>
2017-06-12 11:43:00 -0400

    Theme destroy successful
    Tested with CURL script for theme-destroy

---
+++ scripts/theme-destroy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+API="http://localhost:4741"
+URL_PATH="/themes"
+
+curl "${API}${URL_PATH}/${ID}" \
+  --include \
+  --request DELETE \
+  --header "Authorization: Token token=${TOKEN}"
+
+echo
+
+# TOKEN=04NN9dzu9Wy77knTyOux9cwIPRFstY7IaQ4sXP4ImV8=--mov1dQ3kyjsODRb6IgMv5xjf/4kEv/+/NMmeBJoPqbU= ID=593eb1d923e2fb13e9c26e5f ./scripts/theme-destroy.sh
