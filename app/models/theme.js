ab12a453d02ec5e16a5b7020db1e3a6248e79792
pjliddy <pjliddy@gmail.com>
2017-06-26 13:45:30 -0400

    Testing timestamps
    Timestamps are not synched between Ember and Express

--- app/models/theme.js
+++ app/models/theme.js
@@ -17,15 +17,14 @@ const themeSchema = new mongoose.Schema({
   },
 }, {
   timestamps: true,
-  toJSON: {
-    virtuals: true
-    // ,
+  // toJSON: {
+  //   virtuals: true
+  //   // ,
     // transform: function (doc, ret, options) {
     //   let userId = (options.user && options.user._id) || false;
     //   ret.editable = userId && userId.equals(doc._owner);
     //   return ret;
     // },
-  }
 });
 
 // themeSchema.virtual('length').get(function length() {
